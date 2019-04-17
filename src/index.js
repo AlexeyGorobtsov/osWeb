9f9747ee2b676e5d09216e4f2cac75c5cadc04a8
Alexey <lexa2987@mail.ru>
2019-04-17 19:23:38 +0300

    button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 import './index.css';
+
+const addRippleEffect = function(e) {
+    const target = e.target;
+    if (target.tagName.toLowerCase() !== 'button') return false;
+    const rect = target.getBoundingClientRect();
+    let ripple = target.querySelector('.ripple');
+    if (!ripple) {
+        ripple = document.createElement('span');
+        ripple.className = 'ripple';
+        ripple.style.height = ripple.style.width = Math.max(e.target.offsetWidth, target.offsetHeight) + 'px';
+        target.appendChild(ripple);
+    }
+    ripple.classList.remove('show');
+    const top = e.pageY - rect.top - ripple.offsetHeight / 2 - document.body.scrollTop;
+    const left = e.pageX - rect.left - ripple.offsetWidth / 2 - document.body.scrollLeft;
+    ripple.style.top = top + 'px';
+    ripple.style.left = left + 'px';
+    ripple.classList.add('show');
+    return false;
+};
+
+document.addEventListener('click', addRippleEffect, false);
